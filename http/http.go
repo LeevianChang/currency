f5f4c89935ef870429fff78816409e350e207e17
leevianChang <leevianChang@gmail.com>
2022-05-11 13:30:10 +0800

    no message

--- http/http.go
+++ http/http.go
@@ -16,10 +16,24 @@ import (
 
 func Init(eng *gin.Engine) {
 	eng.POST("/v1/currencies/live", getCurrencyLive)
+	eng.POST("/v1/currencies/list", getCurrencyList)
+	eng.POST("/v1/currencies/timeframe", getCurrencyFrame)
+	eng.POST("/v1/currencies/change", getCurrencyChange)
+
 }
 
 type Params struct {
 	TimeStamp int64 `json:"timestamp"`
+}
+
+type FrameParams struct {
+	TimeStamp int64             `json:"timestamp"`
+	Params    map[string]string `json:"req_params"`
+}
+
+type ChangeParams struct {
+	TimeStamp int64             `json:"timestamp"`
+	Params    map[string]string `json:"req_params"`
 }
 
 const (
@@ -77,7 +91,121 @@ func getCurrencyLive(c *gin.Context) {
 		})
 		return
 	}
-	c.JSON(200, service.CurrencyService.GetCurrency(c, iv))
+	data, t := service.CurrencyService.GetCurrencyLive(c)
+	c.Header(XTimeStamp, fmt.Sprintf("%d", t))
+	c.JSON(200, data)
+}
+
+func getCurrencyList(c *gin.Context) {
+	v := new(struct {
+		Params    string `form:"params" validate:"required"`
+		TimeStamp int64  `form:"timestamp"`
+	})
+	if err := c.Bind(v); err != nil {
+		return
+	}
+
+	header := c.Request.Header
+	if header != nil {
+		times, ok := header[XTimeStamp]
+		if !ok {
+			c.JSON(200, &model.Reply{
+				Message: model.CodeErrTimeMessage,
+				Errno:   model.CodeErrTime,
+			})
+			return
+		}
+		if len(times) == 1 {
+			v.TimeStamp, _ = strconv.ParseInt(times[0], 10, 64)
+		}
+	}
+	now := time.Now().Unix()
+	if v.TimeStamp < now-120 || v.TimeStamp > now+120 {
+		c.JSON(200, &model.Reply{
+			Message: model.CodeErrTimeMessage,
+			Errno:   model.CodeErrTime,
+		})
+		return
+	}
+	iv := service.CurrencyService.GetIV(v.TimeStamp)
+
+	params, err := tool.GcmDecrypt(v.Params, "6143ec9acb9160154306ffb7d12ee141", []byte(iv))
+	if err != nil {
+		c.JSON(200, &model.Reply{
+			Message: model.CodeErrDecryptMessage,
+			Errno:   model.CodeErrDecrypt,
+		})
+		return
+	}
+	p := &Params{}
+	err = json.Unmarshal([]byte(params), p)
+	fmt.Println(p.TimeStamp, v.TimeStamp)
+	if p.TimeStamp != v.TimeStamp {
+		c.JSON(200, &model.Reply{
+			Message: model.CodeErrTimeMessage,
+			Errno:   model.CodeErrTime,
+		})
+		return
+	}
+	data, t := service.CurrencyService.GetCurrencyList(c)
+	c.Header(XTimeStamp, fmt.Sprintf("%d", t))
+	c.JSON(200, data)
+}
+
+func getCurrencyFrame(c *gin.Context) {
+	v := new(struct {
+		Params    string `form:"params" validate:"required"`
+		TimeStamp int64  `form:"timestamp"`
+	})
+	if err := c.Bind(v); err != nil {
+		return
+	}
+
+	header := c.Request.Header
+	if header != nil {
+		times, ok := header[XTimeStamp]
+		if !ok {
+			c.JSON(200, &model.Reply{
+				Message: model.CodeErrTimeMessage,
+				Errno:   model.CodeErrTime,
+			})
+			return
+		}
+		if len(times) == 1 {
+			v.TimeStamp, _ = strconv.ParseInt(times[0], 10, 64)
+		}
+	}
+	now := time.Now().Unix()
+	if v.TimeStamp < now-120 || v.TimeStamp > now+120 {
+		c.JSON(200, &model.Reply{
+			Message: model.CodeErrTimeMessage,
+			Errno:   model.CodeErrTime,
+		})
+		return
+	}
+	iv := service.CurrencyService.GetIV(v.TimeStamp)
+
+	params, err := tool.GcmDecrypt(v.Params, "6143ec9acb9160154306ffb7d12ee141", []byte(iv))
+	if err != nil {
+		c.JSON(200, &model.Reply{
+			Message: model.CodeErrDecryptMessage,
+			Errno:   model.CodeErrDecrypt,
+		})
+		return
+	}
+	p := &FrameParams{}
+	err = json.Unmarshal([]byte(params), p)
+	fmt.Println(p.TimeStamp, v.TimeStamp)
+	if p.TimeStamp != v.TimeStamp {
+		c.JSON(200, &model.Reply{
+			Message: model.CodeErrTimeMessage,
+			Errno:   model.CodeErrTime,
+		})
+		return
+	}
+	data, t := service.CurrencyService.GetCurrencyTimeFrame(c, p.Params)
+	c.Header(XTimeStamp, fmt.Sprintf("%d", t))
+	c.JSON(200, data)
 }
 
 func Limiter(lmt *limiter.Limiter) gin.HandlerFunc {
@@ -93,4 +221,60 @@ func Limiter(lmt *limiter.Limiter) gin.HandlerFunc {
 			c.Next()
 		}
 	}
+}
+
+func getCurrencyChange(c *gin.Context) {
+	v := new(struct {
+		Params    string `form:"params" validate:"required"`
+		TimeStamp int64  `form:"timestamp"`
+	})
+	if err := c.Bind(v); err != nil {
+		return
+	}
+
+	header := c.Request.Header
+	if header != nil {
+		times, ok := header[XTimeStamp]
+		if !ok {
+			c.JSON(200, &model.Reply{
+				Message: model.CodeErrTimeMessage,
+				Errno:   model.CodeErrTime,
+			})
+			return
+		}
+		if len(times) == 1 {
+			v.TimeStamp, _ = strconv.ParseInt(times[0], 10, 64)
+		}
+	}
+	now := time.Now().Unix()
+	if v.TimeStamp < now-120 || v.TimeStamp > now+120 {
+		c.JSON(200, &model.Reply{
+			Message: model.CodeErrTimeMessage,
+			Errno:   model.CodeErrTime,
+		})
+		return
+	}
+	iv := service.CurrencyService.GetIV(v.TimeStamp)
+
+	params, err := tool.GcmDecrypt(v.Params, "6143ec9acb9160154306ffb7d12ee141", []byte(iv))
+	if err != nil {
+		c.JSON(200, &model.Reply{
+			Message: model.CodeErrDecryptMessage,
+			Errno:   model.CodeErrDecrypt,
+		})
+		return
+	}
+	p := &ChangeParams{}
+	err = json.Unmarshal([]byte(params), p)
+	fmt.Println(p.TimeStamp, v.TimeStamp)
+	if p.TimeStamp != v.TimeStamp {
+		c.JSON(200, &model.Reply{
+			Message: model.CodeErrTimeMessage,
+			Errno:   model.CodeErrTime,
+		})
+		return
+	}
+	data, t := service.CurrencyService.GetCurrencyTimeChange(c, p.Params)
+	c.Header(XTimeStamp, fmt.Sprintf("%d", t))
+	c.JSON(200, data)
 }
--- service/service.go
+++ service/service.go
@@ -17,15 +17,19 @@ import (
 var CurrencyService *Service
 
 type Service struct {
-	Currency *Currency
+	CurrencyLive *CurrencyLive
+	CurrencyList *CurrencyList
 }
 
 const (
-	accessKey = "a3f9c33d76d640112a2c3d3fd83e456d"
-	url       = "http://api.currencylayer.com/live"
+	accessKey    = "8WNG6RGp8vKEJPFnzvX23JwGGDaGupyw"
+	liveUrl      = "https://api.apilayer.com/currency_data/live"
+	listUrl      = "https://api.apilayer.com/currency_data/list"
+	timeFrameUrl = "https://api.apilayer.com/currency_data/timeframe"
+	changeUrl    = "https://api.apilayer.com/currency_data/change"
 )
 
-type Currency struct {
+type CurrencyLive struct {
 	Success   bool               `json:"success"`
 	Terms     string             `json:"terms"`
 	Privacy   string             `json:"privacy"`
@@ -34,17 +38,49 @@ type Currency struct {
 	Quotes    map[string]float64 `json:"quotes"`
 }
 
+type CurrencyTimeFrame struct {
+	EndDate   string                 `json:"end_date"`
+	Quotes    map[string]interface{} `json:"quotes"`
+	Source    string                 `json:"source"`
+	StartDate string                 `json:"start_date"`
+	Success   bool                   `json:"success"`
+	TimeFrame bool                   `json:"timeframe"`
+}
+
+type CurrencyTimeChange struct {
+	Source    string                 `json:"source"`
+	StartDate string                 `json:"start_date"`
+	Change    bool                   `json:"change"`
+	Quotes    map[string]interface{} `json:"quotes"`
+	EndDate   string                 `json:"end_date"`
+	Success   bool                   `json:"success"`
+}
+
+type CurrencyList struct {
+	Success bool              `json:"success"`
+	Symbols map[string]string `json:"symbols"`
+}
+
 func Init() {
 	CurrencyService = &Service{}
 	ctx := context.Background()
-	CurrencyService.HttpGetCurrency(ctx)
-	go CurrencyService.LoopGetCurrency(ctx)
+	CurrencyService.HttpGetCurrencyLive(ctx)
+	CurrencyService.HttpGetCurrencyList(ctx)
+	go CurrencyService.LoopGetCurrencyLive(ctx)
+	go CurrencyService.LoopGetCurrencyList(ctx)
 }
 
-func (s *Service) LoopGetCurrency(ctx context.Context) {
-	ticker := time.NewTicker(10 * time.Minute)
+func (s *Service) LoopGetCurrencyLive(ctx context.Context) {
+	ticker := time.NewTicker(30 * time.Minute)
 	for range ticker.C {
-		go CurrencyService.HttpGetCurrency(ctx)
+		go CurrencyService.HttpGetCurrencyLive(ctx)
+	}
+}
+
+func (s *Service) LoopGetCurrencyList(ctx context.Context) {
+	ticker := time.NewTicker(30 * time.Minute)
+	for range ticker.C {
+		go CurrencyService.HttpGetCurrencyList(ctx)
 	}
 }
 
@@ -70,18 +106,20 @@ func (s *Service) GetIV(timeStamp int64) (res string) {
 	return
 }
 
-func (s *Service) GetCurrency(ctx context.Context, iv string) (res *model.Reply) {
+func (s *Service) GetCurrencyLive(ctx context.Context) (res *model.Reply, timestamp int64) {
 	data := make(map[string]interface{})
-	currency, err := json.Marshal(s.Currency)
+	currency, err := json.Marshal(s.CurrencyLive)
 	if err != nil {
 		return
 	}
+	timestamp = s.CurrencyLive.Timestamp
+	iv := s.GetIV(timestamp)
 	b, err := tool.GcmEncrypt(string(currency), "6143ec9acb9160154306ffb7d12ee141", []byte(iv))
 	if err != nil {
 		return &model.Reply{
 			Errno:   model.CodeErrEncrypt,
 			Message: model.CodeErrEncryptMessage,
-		}
+		}, 0
 	}
 	data["ed"] = b
 	res = &model.Reply{
@@ -89,26 +127,175 @@ func (s *Service) GetCurrency(ctx context.Context, iv string) (res *model.Reply)
 	}
 	return
 }
-func (s *Service) HttpGetCurrency(ctx context.Context) (code int, res *Currency) {
-	// 处理query
-	query := fmt.Sprintf("?access_key=%s", accessKey)
 
-	req, _ := http.NewRequest("GET", url+query, nil)
+func (s *Service) GetCurrencyList(ctx context.Context) (res *model.Reply, timestamp int64) {
+	data := make(map[string]interface{})
+	currency, err := json.Marshal(s.CurrencyList)
+	if err != nil {
+		return
+	}
+	timestamp = time.Now().Unix()
+	iv := s.GetIV(timestamp)
+	b, err := tool.GcmEncrypt(string(currency), "6143ec9acb9160154306ffb7d12ee141", []byte(iv))
+	if err != nil {
+		return &model.Reply{
+			Errno:   model.CodeErrEncrypt,
+			Message: model.CodeErrEncryptMessage,
+		}, 0
+	}
+	data["ed"] = b
+	res = &model.Reply{
+		Data: data,
+	}
+	return
+}
+
+func (s *Service) HttpGetCurrencyLive(ctx context.Context) (code int, res *CurrencyLive) {
+	// 处理query
+	//query := fmt.Sprintf("?access_key=%s", accessKey)
+	req, _ := http.NewRequest("GET", liveUrl, nil)
 	// 处理header
 	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
 	req.Header.Set("Connection", "keep-alive")
 	req.Header.Set("Host", "api.currencylayer.com")
 	req.Header.Set("Accept", "gzip, deflate")
 	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36")
+	req.Header.Set("apikey", accessKey)
+	//req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36")
 	resp, err := (&http.Client{Timeout: time.Millisecond * 5000}).Do(req)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
-	json.Unmarshal(body, &s.Currency)
-	fmt.Println(s.Currency)
-	res = s.Currency
+	json.Unmarshal(body, &s.CurrencyLive)
+	fmt.Println(s.CurrencyLive)
+	res = s.CurrencyLive
+	return
+}
+
+func (s *Service) HttpGetCurrencyList(ctx context.Context) (code int, res *CurrencyList) {
+	// 处理query
+	//query := fmt.Sprintf("?access_key=%s", accessKey)
+	req, _ := http.NewRequest("GET", listUrl, nil)
+	// 处理header
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("Host", "api.currencylayer.com")
+	req.Header.Set("Accept", "gzip, deflate")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7")
+	req.Header.Set("apikey", accessKey)
+	//req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36")
+	resp, err := (&http.Client{Timeout: time.Millisecond * 5000}).Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(body, &s.CurrencyList)
+	fmt.Println(s.CurrencyList)
+	res = s.CurrencyList
+	return
+}
+
+func (s *Service) GetCurrencyTimeFrame(ctx context.Context, params map[string]string) (res *model.Reply, timestamp int64) {
+	data := make(map[string]interface{})
+	_, d := s.HttpGetCurrencyTimeFrame(ctx, params)
+	currency, err := json.Marshal(d)
+	if err != nil {
+		return
+	}
+	timestamp = time.Now().Unix()
+	iv := s.GetIV(timestamp)
+	fmt.Println(currency, "currency")
+	b, err := tool.GcmEncrypt(string(currency), "6143ec9acb9160154306ffb7d12ee141", []byte(iv))
+	if err != nil {
+		return &model.Reply{
+			Errno:   model.CodeErrEncrypt,
+			Message: model.CodeErrEncryptMessage,
+		}, 0
+	}
+	data["ed"] = b
+	res = &model.Reply{
+		Data: data,
+	}
+	return
+}
+
+func (s *Service) GetCurrencyTimeChange(ctx context.Context, params map[string]string) (res *model.Reply, timestamp int64) {
+	data := make(map[string]interface{})
+	_, d := s.HttpGetCurrencyChange(ctx, params)
+	currency, err := json.Marshal(d)
+	if err != nil {
+		return
+	}
+	timestamp = time.Now().Unix()
+	iv := s.GetIV(timestamp)
+	fmt.Println(currency, "currency")
+	b, err := tool.GcmEncrypt(string(currency), "6143ec9acb9160154306ffb7d12ee141", []byte(iv))
+	if err != nil {
+		return &model.Reply{
+			Errno:   model.CodeErrEncrypt,
+			Message: model.CodeErrEncryptMessage,
+		}, 0
+	}
+	data["ed"] = b
+	res = &model.Reply{
+		Data: data,
+	}
+	return
+}
+
+func (s *Service) HttpGetCurrencyTimeFrame(ctx context.Context, params map[string]string) (code int, res *CurrencyTimeFrame) {
+	// 处理query
+	var q string
+	for k, v := range params {
+		q += fmt.Sprintf("%s=%s&", k, v)
+	}
+	q = "?" + q
+	req, _ := http.NewRequest("GET", timeFrameUrl+q, nil)
+	// 处理header
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("Host", "api.currencylayer.com")
+	req.Header.Set("Accept", "gzip, deflate")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7")
+	req.Header.Set("apikey", accessKey)
+	//req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36")
+	resp, err := (&http.Client{Timeout: time.Millisecond * 1000}).Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	res = &CurrencyTimeFrame{}
+	json.Unmarshal(body, res)
+	return
+}
+
+func (s *Service) HttpGetCurrencyChange(ctx context.Context, params map[string]string) (code int, res *CurrencyTimeChange) {
+	// 处理query
+	var q string
+	for k, v := range params {
+		q += fmt.Sprintf("%s=%s&", k, v)
+	}
+	q = "?" + q
+	req, _ := http.NewRequest("GET", changeUrl+q, nil)
+	// 处理header
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("Host", "api.currencylayer.com")
+	req.Header.Set("Accept", "gzip, deflate")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7")
+	req.Header.Set("apikey", accessKey)
+	//req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36")
+	resp, err := (&http.Client{Timeout: time.Millisecond * 1000}).Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	res = &CurrencyTimeChange{}
+	json.Unmarshal(body, res)
 	return
 }
